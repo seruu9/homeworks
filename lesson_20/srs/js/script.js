9a8604a15805447a7d193b0cdb8fd5ede9b0dcdc
seruu9 <52935796+seruu9@users.noreply.github.com>
2019-08-10 13:32:04 +0300

    Add files via upload

---
+++ lesson_20/srs/js/script.js
@@ -0,0 +1,37 @@
+﻿// оброботка и отправка формы ччерез Ajax
+$('#offer-form').on('submit', function(event){
+  event.preventDefault();
+  $.ajax({
+    url: 'mail.php',
+    type: 'POST',
+    data: $(this).serialize(),
+    success: function(data) {
+      $('.success').text(data + ', Ваша форма отправлена.')
+    }
+  });
+});
+// слайдер 
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
